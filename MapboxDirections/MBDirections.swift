86645a17fd1105187fff2ef162a11ec38476d620
Austin <austinbrown34@gmail.com>
2018-08-21 06:16:35 -0600

    fix parameter order

--- MapboxDirections/MBDirections.swift
+++ MapboxDirections/MBDirections.swift
@@ -10,18 +10,18 @@ let defaultAccessToken = Bundle.main.object(forInfoDictionaryKey: "MGLMapboxAcce
 /// The user agent string for any HTTP requests performed directly within this library.
 let userAgent: String = {
     var components: [String] = []
-    
+
     if let appName = Bundle.main.infoDictionary?["CFBundleName"] as? String ?? Bundle.main.infoDictionary?["CFBundleIdentifier"] as? String {
         let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
         components.append("\(appName)/\(version)")
     }
-    
+
     let libraryBundle: Bundle? = Bundle(for: Directions.self)
-    
+
     if let libraryName = libraryBundle?.infoDictionary?["CFBundleName"] as? String, let version = libraryBundle?.infoDictionary?["CFBundleShortVersionString"] as? String {
         components.append("\(libraryName)/\(version)")
     }
-    
+
     let system: String
     #if os(OSX)
         system = "macOS"
@@ -36,7 +36,7 @@ let userAgent: String = {
     #endif
     let systemVersion = ProcessInfo().operatingSystemVersion
     components.append("\(system)/\(systemVersion.majorVersion).\(systemVersion.minorVersion).\(systemVersion.patchVersion)")
-    
+
     let chip: String
     #if arch(x86_64)
         chip = "x86_64"
@@ -48,7 +48,7 @@ let userAgent: String = {
         chip = "i386"
     #endif
     components.append("(\(chip))")
-    
+
     return components.joined(separator: " ")
 }()
 
@@ -60,7 +60,7 @@ extension CLLocationCoordinate2D {
         assert(array.count == 2)
         self.init(latitude: array[1], longitude: array[0])
     }
-    
+
     /**
      Initializes a coordinate pair based on the given GeoJSON point object.
      */
@@ -68,7 +68,7 @@ extension CLLocationCoordinate2D {
         assert(point["type"] as? String == "Point")
         self.init(geoJSON: point["coordinates"] as! [Double])
     }
-    
+
     internal static func coordinates(geoJSON lineString: JSONDictionary) -> [CLLocationCoordinate2D] {
         let type = lineString["type"] as? String
         assert(type == "LineString" || type == "Point")
@@ -88,105 +88,105 @@ extension CLLocation {
 
 /**
  A `Directions` object provides you with optimal directions between different locations, or waypoints. The directions object passes your request to the [Mapbox Directions API](https://www.mapbox.com/api-documentation/?language=Swift#directions) and returns the requested information to a closure (block) that you provide. A directions object can handle multiple simultaneous requests. A `RouteOptions` object specifies criteria for the results, such as intermediate waypoints, a mode of transportation, or the level of detail to be returned.
- 
+
  Each result produced by the directions object is stored in a `Route` object. Depending on the `RouteOptions` object you provide, each route may include detailed information suitable for turn-by-turn directions, or it may include only high-level information such as the distance, estimated travel time, and name of each leg of the trip. The waypoints that form the request may be conflated with nearby locations, as appropriate; the resulting waypoints are provided to the closure.
  */
 @objc(MBDirections)
 open class Directions: NSObject {
     /**
      A closure (block) to be called when a directions request is complete.
-     
+
      - parameter waypoints: An array of `Waypoint` objects. Each waypoint object corresponds to a `Waypoint` object in the original `RouteOptions` object. The locations and names of these waypoints are the result of conflating the original waypoints to known roads. The waypoints may include additional information that was not specified in the original waypoints.
-        
+
         If the request was canceled or there was an error obtaining the routes, this parameter may be `nil`.
      - parameter routes: An array of `Route` objects. The preferred route is first; any alternative routes come next if the `RouteOptions` object’s `includesAlternativeRoutes` property was set to `true`. The preferred route depends on the route options object’s `profileIdentifier` property.
-        
+
         If the request was canceled or there was an error obtaining the routes, this parameter is `nil`. This is not to be confused with the situation in which no results were found, in which case the array is present but empty.
      - parameter error: The error that occurred, or `nil` if the placemarks were obtained successfully.
      */
     public typealias RouteCompletionHandler = (_ waypoints: [Waypoint]?, _ routes: [Route]?, _ error: NSError?) -> Void
-    
+
     /**
      A closure (block) to be called when a map matching request is complete.
-     
+
      If the request was canceled or there was an error obtaining the matches, this parameter is `nil`. This is not to be confused with the situation in which no matches were found, in which case the array is present but empty.
      - parameter error: The error that occurred, or `nil` if the placemarks were obtained successfully.
      */
     public typealias MatchCompletionHandler = (_ matches: [Match]?, _ error: NSError?) -> Void
-    
+
     // MARK: Creating a Directions Object
-    
+
     /**
      The shared directions object.
-     
+
      To use this object, a Mapbox [access token](https://www.mapbox.com/help/define-access-token/) should be specified in the `MGLMapboxAccessToken` key in the main application bundle’s Info.plist.
      */
     @objc(sharedDirections)
     open static let shared = Directions(accessToken: nil)
-    
+
     /// The API endpoint to request the directions from.
     internal var apiEndpoint: URL
-    
+
     /// The Mapbox access token to associate the request with.
     internal let accessToken: String
-    
+
     /**
      Initializes a newly created directions object with an optional access token and host.
-     
+
      - parameter accessToken: A Mapbox [access token](https://www.mapbox.com/help/define-access-token/). If an access token is not specified when initializing the directions object, it should be specified in the `MGLMapboxAccessToken` key in the main application bundle’s Info.plist.
      - parameter host: An optional hostname to the server API. The [Mapbox Directions API](https://www.mapbox.com/api-documentation/?language=Swift#directions) endpoint is used by default.
      */
     @objc public init(accessToken: String?, host: String?) {
         let accessToken = accessToken ?? defaultAccessToken
         assert(accessToken != nil && !accessToken!.isEmpty, "A Mapbox access token is required. Go to <https://www.mapbox.com/studio/account/tokens/>. In Info.plist, set the MGLMapboxAccessToken key to your access token, or use the Directions(accessToken:host:) initializer.")
-        
+
         self.accessToken = accessToken!
-        
+
         var baseURLComponents = URLComponents()
         baseURLComponents.scheme = "https"
         baseURLComponents.host = host ?? "api.mapbox.com"
         self.apiEndpoint = baseURLComponents.url!
     }
-    
+
     /**
      Initializes a newly created directions object with an optional access token.
-     
+
      The directions object sends requests to the [Mapbox Directions API](https://www.mapbox.com/api-documentation/?language=Swift#directions) endpoint.
-     
+
      - parameter accessToken: A Mapbox [access token](https://www.mapbox.com/help/define-access-token/). If an access token is not specified when initializing the directions object, it should be specified in the `MGLMapboxAccessToken` key in the main application bundle’s Info.plist.
      */
     @objc public convenience init(accessToken: String?) {
         self.init(accessToken: accessToken, host: nil)
     }
-    
+
     // MARK: Getting Directions
-    
+
     /**
      - (void)getJSON:(CLLocationCoordinate2D *)start end:(CLLocationCoordinate2D *)end xmlpath:(NSString *)xmlpath{
-     
+
      NSString* sourcePath = [[NSBundle mainBundle] pathForResource:@"billings2" ofType:@"xml"];
      //    NSString* sourcePath = [[NSBundle mainBundle] pathForResource:@"billings2" inDirectory:@"billings" ofType:@"xml"];
-     
-     
+
+
      //    NSString *documentDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
-     
+
      //    NSString *documentPath = [NSString stringWithFormat:@"%@/%@%s", documentDir, xmlpath, ".osrm"];
      NSString *documentPath = [NSString stringWithFormat:@"%@%s", sourcePath, ".osrm"];
-     
+
      //    NSLog(@"PATH: %@", sourcePath);
-     
+
      RouteService *routeService = [[RouteService alloc] initWithMapData: documentPath];
-     
+
      routeService.overview = ORSMOverviewFull;
-     
+
      routeService.geometries = ORSMGeometryGeoJSON;
      routeService.steps = true;
      NSDictionary<NSString *, NSObject *> *jsonResult;
-     
-     
+
+
      jsonResult = [routeService getRoutesFrom:*start to:*end];
      NSLog(@"%@", jsonResult);
-     
+
      NSMutableArray *newroutes = [[NSMutableArray alloc] init];
      for (NSObject *route in [jsonResult valueForKeyPath:@"routes"]) {
      [route setValue:@"en-US" forKey:@"voiceLocale"];
@@ -196,19 +196,19 @@ open class Directions: NSObject {
      //            [self setCurrentMessage: routeMessage];
      //            NSLengthFormatter *distanceFormatter = [[NSLengthFormatter alloc] init];
      //            NSString *formattedDistance = [distanceFormatter stringFromMeters: leg.distance];
-     
+
      //            NSDateComponentsFormatter *travelTimeFormatter = [[NSDateComponentsFormatter alloc] init];
-     
+
      //            travelTimeFormatter.unitsStyle = NSDateComponentsFormatterUnitsStyleShort;
      //            NSString *formattedTravelTime = [travelTimeFormatter stringFromTimeInterval: route.expectedTravelTime];
      //            NSLog(@"Distance: %@; ETA: %@", formattedDistance, formattedTravelTime);
-     
+
      //            [routeMessage appendFormat:@"Distance: %@; ETA: %@\n\n", formattedDistance, formattedTravelTime];
      NSMutableArray *newsteps = [[NSMutableArray alloc] init];
      for (NSObject *step in [leg valueForKey:@"steps"]){
      NSMutableArray *voiceInstructions = [[NSMutableArray alloc] init];
      NSMutableDictionary *voiceObject = [[NSMutableDictionary alloc] init];
-     
+
      OSRMInstructionFormatter *osrminstructionFormatter = [[OSRMInstructionFormatter alloc] initWithVersion:@"v5"];
      //                [distanceFormatter setUnitStyle:NSFormattingUnitStyleMedium];
      //                NSLog(@"%@", [osrminstructionFormatter stringForObjectValue:step]);
@@ -237,19 +237,19 @@ open class Directions: NSObject {
      }
      [leg setValue:newsteps forKeyPath:@"steps" ];
      [newlegs addObject:leg];
-     
-     
+
+
      }
      [route setValue:newlegs forKeyPath:@"legs"];
      [newroutes addObject:route];
      }
-     
-     
+
+
      }
      */
 //    @objc (getJSON:start)
     @discardableResult open func getJSON(_ start: CLLocationCoordinate2D, end: CLLocationCoordinate2D, osrmPath: String) -> Dictionary<String, Any> {
-        
+
         let routeService = RouteService.init(mapData: osrmPath)
         routeService?.overview = .full
         routeService?.geometries = .geoJSON
@@ -265,7 +265,7 @@ open class Directions: NSObject {
 //        }
 //        let newjsonResult = jsonResult as Dictionary
 //        let routes = jsonResult.valueForKeyPath("routes")
-        
+
         for r in jsonResult!["routes"] as! [Dictionary<String, Any>]{
             var route = r
             route["voiceLocal"] = "en-US"
@@ -287,7 +287,7 @@ open class Directions: NSObject {
                     var newstep = step
                     newstep["voiceInstructions"] = voiceInstructions
                     newsteps.append(newstep)
-                    
+
                 }
                 var newleg = leg
                 newleg["steps"] = newsteps
@@ -299,7 +299,7 @@ open class Directions: NSObject {
         var newjsonResult = jsonResult
         newjsonResult!["routes"] = newroutes as NSObject
 //        jsonResult["routes"] = newroutes
-        
+
 //        NSMutableArray *newroutes = [[NSMutableArray alloc] init];
 //        for (NSObject *route in [jsonResult valueForKeyPath:@"routes"]) {
 //            [route setValue:@"en-US" forKey:@"voiceLocale"];
@@ -342,16 +342,16 @@ open class Directions: NSObject {
     }
     /**
      Begins asynchronously calculating the route or routes using the given options and delivers the results to a closure.
-     
+
      This method retrieves the routes asynchronously over a network connection. If a connection error or server error occurs, details about the error are passed into the given completion handler in lieu of the routes.
-     
+
      Routes may be displayed atop a [Mapbox map](https://www.mapbox.com/maps/). They may be cached but may not be stored permanently. To use the results in other contexts or store them permanently, [upgrade to a Mapbox enterprise plan](https://www.mapbox.com/directions/#pricing).
-     
+
      - parameter options: A `RouteOptions` object specifying the requirements for the resulting routes.
      - parameter completionHandler: The closure (block) to call with the resulting routes. This closure is executed on the application’s main thread.
      - returns: The data task used to perform the HTTP request. If, while waiting for the completion handler to execute, you no longer want the resulting routes, cancel this task.
      */
-    @objc(calculateDirectionsWithOptions:completionHandler:osrmPath:)
+    @objc(calculateDirectionsWithOptions:osrmPath:completionHandler:)
     @discardableResult open func calculate(_ options: RouteOptions, osrmPath: String? = nil, completionHandler: @escaping RouteCompletionHandler) -> URLSessionDataTask {
         let url = self.url(forCalculating: options)
         let task = dataTask(with: url, completionHandler: { (json) in
@@ -366,7 +366,7 @@ open class Directions: NSObject {
             completionHandler(response.0, response.1, nil)
         }) { (error) in
 //
-            
+
             if osrmPath == nil{
                 completionHandler(nil, nil, error)
             }
@@ -379,16 +379,16 @@ open class Directions: NSObject {
 //            let xmlpath
 //            let documentsDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
 //            let osrmPath = documentsDir + xmlpath + ".osrm"
-            
+
         }
         task.resume()
         return task
     }
-    
+
     /**
      Begins asynchronously calculating a match using the given options and delivers the results to a closure.
-     
-     
+
+
      - parameter options: A `MatchOptions` object specifying the requirements for the resulting match.
      - parameter completionHandler: The closure (block) to call with the resulting routes. This closure is executed on the application’s main thread.
      - returns: The data task used to perform the HTTP request. If, while waiting for the completion handler to execute, you no longer want the resulting routes, cancel this task.
@@ -413,7 +413,7 @@ open class Directions: NSObject {
         task.resume()
         return task
     }
-    
+
     @objc(calculateRoutesMatchingOptions:completionHandler:)
     @discardableResult open func calculateRoutes(matching options: MatchOptions, completionHandler: @escaping RouteCompletionHandler) -> URLSessionDataTask {
         let url = self.url(forCalculating: options)
@@ -434,10 +434,10 @@ open class Directions: NSObject {
         task.resume()
         return task
     }
-    
+
     /**
      Returns a URL session task for the given URL that will run the given closures on completion or error.
-     
+
      - parameter url: The URL to request.
      - parameter completionHandler: The closure to call with the parsed JSON response dictionary.
      - parameter errorHandler: The closure to call when there is an error.
@@ -445,16 +445,16 @@ open class Directions: NSObject {
      - postcondition: The caller must resume the returned task.
      */
     fileprivate func dataTask(with url: URL, data: Data? = nil, completionHandler: @escaping (_ json: JSONDictionary) -> Void, errorHandler: @escaping (_ error: NSError) -> Void) -> URLSessionDataTask {
-        
+
         var request = URLRequest(url: url)
         request.setValue(userAgent, forHTTPHeaderField: "User-Agent")
-        
+
         if let data = data {
             request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
             request.httpMethod = "POST"
             request.httpBody = data
         }
-        
+
         return URLSession.shared.dataTask(with: request as URLRequest) { (data, response, error) in
             var json: JSONDictionary = [:]
             if let data = data, response?.mimeType == "application/json" {
@@ -464,7 +464,7 @@ open class Directions: NSObject {
                     assert(false, "Invalid data")
                 }
             }
-            
+
             let apiStatusCode = json["code"] as? String
             let apiMessage = json["message"] as? String
             guard data != nil && error == nil && ((apiStatusCode == nil && apiMessage == nil) || apiStatusCode == "Ok") else {
@@ -474,16 +474,16 @@ open class Directions: NSObject {
                 }
                 return
             }
-            
+
             DispatchQueue.main.async {
                 completionHandler(json)
             }
         }
     }
-    
+
     /**
      The HTTP URL used to fetch the routes from the API.
-     
+
      After requesting the URL returned by this method, you can parse the JSON data in the response and pass it into the `Route.init(json:waypoints:profileIdentifier:)` initializer.
      */
     @objc(URLForCalculatingDirectionsWithOptions:)
@@ -491,13 +491,13 @@ open class Directions: NSObject {
         let params = options.params + [
             URLQueryItem(name: "access_token", value: accessToken),
         ]
-        
+
         let unparameterizedURL = URL(string: options.path, relativeTo: apiEndpoint)!
         var components = URLComponents(url: unparameterizedURL, resolvingAgainstBaseURL: true)!
         components.queryItems = params
         return components.url!
     }
-    
+
     /**
      Returns an error that supplements the given underlying error with additional information from the an HTTP response’s body or headers.
      */
@@ -550,14 +550,14 @@ extension HTTPURLResponse {
         }
         return UInt(limit)
     }
-    
+
     var rateLimitInterval: TimeInterval? {
         guard let interval = allHeaderFields["X-Rate-Limit-Interval"] as? String else {
             return nil
         }
         return TimeInterval(interval)
     }
-    
+
     var rateLimitResetTime: Date? {
         guard let resetTime = allHeaderFields["X-Rate-Limit-Reset"] as? String else {
             return nil
